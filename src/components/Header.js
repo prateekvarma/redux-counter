0cce2fbb3194b0722d0af3f857b447a72cc7d8f4
Prateek Varma <prtk.varma@gmail.com>
2022-10-24 13:54:23 -0600

    Implemented login and logout, interacting with Redux toolkit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { authActions } from '../store';
 import classes from './Header.module.css';
 
 const Header = () => {
-
+  const dispatch = useDispatch();
   const isAuth = useSelector((state) => state.auth.isAuthenticated);
   
+  const logoutHandler = (event) => {
+    dispatch(authActions.logout());
+  };
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
@@ -17,7 +22,7 @@ const Header = () => {
             <a href='/'>My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            <button onClick={logoutHandler}>Logout</button>
           </li>
         </ul>
       </nav>}
